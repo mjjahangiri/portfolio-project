d21c754bdf3a9bfbe767e2ebd5b5838d702512cb
MJJ <mj.jahangiri.6@gmail.com>
2022-01-24 23:01:53 +0330

    Updates

---
+++ precache-manifest.a01faae3d5be4854c1df0ca601c85e54.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c559d265d70c3842315cd3887b27cf8",
+    "url": "/portfolio/build/index.html"
+  },
+  {
+    "revision": "2b6a30447e27a283ee9c",
+    "url": "/portfolio/build/static/css/2.e7235f0e.chunk.css"
+  },
+  {
+    "revision": "4bc347728c42c669cfac",
+    "url": "/portfolio/build/static/css/main.26c72afc.chunk.css"
+  },
+  {
+    "revision": "2b6a30447e27a283ee9c",
+    "url": "/portfolio/build/static/js/2.7766e2d0.chunk.js"
+  },
+  {
+    "revision": "4aa839f25784e6ec59906b932c7e8c9f",
+    "url": "/portfolio/build/static/js/2.7766e2d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed43a0854f27f6d794a1",
+    "url": "/portfolio/build/static/js/3.978bb521.chunk.js"
+  },
+  {
+    "revision": "4bc347728c42c669cfac",
+    "url": "/portfolio/build/static/js/main.1624f838.chunk.js"
+  },
+  {
+    "revision": "d80d01f08171b6459874",
+    "url": "/portfolio/build/static/js/runtime-main.72996a2b.js"
+  },
+  {
+    "revision": "96e5f6d966afa4b5569698485242223f",
+    "url": "/portfolio/build/static/media/MJJ.96e5f6d9.png"
+  },
+  {
+    "revision": "c8b88509e3bf069fbec6cfaa264be184",
+    "url": "/portfolio/build/static/media/contact.c8b88509.gif"
+  }
+]);
