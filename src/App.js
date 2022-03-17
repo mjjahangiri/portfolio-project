161dda8f6e2dd94847ae530d033d0ea9179c3d1e
MJJ <mj.jahangiri.6@gmail.com>
2022-03-17 23:04:03 +0330

    edit home page and redesigned and change menu svg icons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import './main.css';
-import React from 'react';
-import Home from './Pages/main';
-import Contact from './Pages/contact';
-import Blog from './Pages/blog';
-import Portfolio from './Pages/portfolio';
-import Resume from './Pages/resume';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/main.jsx";
+import Contact from "./Pages/contact.jsx";
+import Portfolio from "./Pages/portfolio.jsx";
+import Resume from "./Pages/resume.jsx";
+import Education from "./Pages/Education";
+import Experience from "./Pages/Experience";
+import Skills from "./Pages/Skills";
+import "./App.css";
+import "./main.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 function App() {
   return (
@@ -14,9 +17,11 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/contact" element={<Contact />} />
-        {/* <Route exact path="/blog" element={<Blog />} /> */}
         <Route exact path="/resume" element={<Resume />} />
-        <Route exact path="/portfolio" element={<Home />} />
+        <Route exact path="/portfolio" element={<Portfolio />} />
+        <Route exact path="/education" element={<Education />} />
+        <Route exact path="/experience" element={<Experience />} />
+        <Route exact path="/skills" element={<Skills />} />
       </Routes>
     </>
   );
