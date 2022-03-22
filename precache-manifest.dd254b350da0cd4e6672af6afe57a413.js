4c8e2febb3fb43420bf9bc99ac4ae95562ca642b
MJJ <mj.jahangiri.6@gmail.com>
2022-03-22 04:48:17 +0430

    Updates

---
+++ precache-manifest.dd254b350da0cd4e6672af6afe57a413.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87a03b309385213c92d51f4f99e6521a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3b90ff68b4f471f185af",
+    "url": "/portfolio/static/css/2.5bd9aba0.chunk.css"
+  },
+  {
+    "revision": "7fd99f4072c9b3f1dd13",
+    "url": "/portfolio/static/css/main.285df0e1.chunk.css"
+  },
+  {
+    "revision": "3b90ff68b4f471f185af",
+    "url": "/portfolio/static/js/2.4519a4de.chunk.js"
+  },
+  {
+    "revision": "4aa839f25784e6ec59906b932c7e8c9f",
+    "url": "/portfolio/static/js/2.4519a4de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ca8c4f67978aa9e3faf",
+    "url": "/portfolio/static/js/3.a24d7a9c.chunk.js"
+  },
+  {
+    "revision": "7fd99f4072c9b3f1dd13",
+    "url": "/portfolio/static/js/main.5b5d72a7.chunk.js"
+  },
+  {
+    "revision": "f0f6aedc4b3d70006870",
+    "url": "/portfolio/static/js/runtime-main.d5da7cc2.js"
+  },
+  {
+    "revision": "f52e23990b71eae472078f9b361e1788",
+    "url": "/portfolio/static/media/Contact.f52e2399.svg"
+  },
+  {
+    "revision": "e8950e29b172f8813124a41861687643",
+    "url": "/portfolio/static/media/Education.e8950e29.svg"
+  },
+  {
+    "revision": "d2727ae794b36b625c5df67fe2f7551e",
+    "url": "/portfolio/static/media/Education1.d2727ae7.png"
+  },
+  {
+    "revision": "3d779efbdd1f56208cc888a1851e5f33",
+    "url": "/portfolio/static/media/Experience.3d779efb.svg"
+  },
+  {
+    "revision": "44534e294da1018fc920fdba2306734e",
+    "url": "/portfolio/static/media/ExperienceAvatar.44534e29.png"
+  },
+  {
+    "revision": "8c7043ee54bd718e2f447900e3d66c74",
+    "url": "/portfolio/static/media/Home.8c7043ee.svg"
+  },
+  {
+    "revision": "96e5f6d966afa4b5569698485242223f",
+    "url": "/portfolio/static/media/MJJ.96e5f6d9.png"
+  },
+  {
+    "revision": "ea833365a6ec08aee221b5fdb2e1f0c8",
+    "url": "/portfolio/static/media/OtaghBazargani.ea833365.png"
+  },
+  {
+    "revision": "bfa40ff1f4713003bd85249c673bf7de",
+    "url": "/portfolio/static/media/PigGame.bfa40ff1.png"
+  },
+  {
+    "revision": "e0bbbac763b3a81af42f4c45f4d6b45e",
+    "url": "/portfolio/static/media/Portfolio.e0bbbac7.svg"
+  },
+  {
+    "revision": "c1fffb62f5b957a74f09f3e184aaa07b",
+    "url": "/portfolio/static/media/Resume.c1fffb62.svg"
+  },
+  {
+    "revision": "8f48a9bea250102ea7cd5f9ff0bc1b50",
+    "url": "/portfolio/static/media/Skills.8f48a9be.svg"
+  },
+  {
+    "revision": "7bdc1a6e8a339c9c84b8c75dbacdf27f",
+    "url": "/portfolio/static/media/SkillsAvatar.7bdc1a6e.svg"
+  },
+  {
+    "revision": "e13923e98addcf389d03c0acb8089fe2",
+    "url": "/portfolio/static/media/Vazir.e13923e9.woff"
+  },
+  {
+    "revision": "9cc4f2a93b1e477492f2eda539a77d59",
+    "url": "/portfolio/static/media/VidlyApp.9cc4f2a9.png"
+  },
+  {
+    "revision": "1ed478a6b265d4b4f5c26bb063203588",
+    "url": "/portfolio/static/media/bootstrap-icons.1ed478a6.woff2"
+  },
+  {
+    "revision": "e4cc83ddcec34db65724eb56c5230b4c",
+    "url": "/portfolio/static/media/bootstrap-icons.e4cc83dd.woff"
+  },
+  {
+    "revision": "c6f46673e2449eb071e2c098c5190c2a",
+    "url": "/portfolio/static/media/iransans.c6f46673.woff2"
+  },
+  {
+    "revision": "c8b88509e3bf069fbec6cfaa264be184",
+    "url": "/portfolio/static/media/telegram.c8b88509.gif"
+  }
+]);
